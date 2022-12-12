90ae8b53c02a7d8a3abc73c3840f081301d332cb
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-12-12 09:26:43 +0700

    add navigatorkey
    handle expired token because refresh token is still prepared by backend

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,10 +7,14 @@ import 'views/splash/splash_view.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: SplashView.routeName,
+      navigatorKey: navigatorKey,
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
         textSelectionTheme: const TextSelectionThemeData(
