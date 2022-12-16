a1abb3d985c3dade0f43e3bfdc55d6864ba01f98
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-12-16 16:42:21 +0700

    fix new error login response

--- lib/src/configs/api/api_config.dart
+++ lib/src/configs/api/api_config.dart
@@ -18,7 +18,11 @@ class APIConfig {
           return handler.next(options);
         },
         onError: (e, handler) async {
-          if (e.response!.statusCode == 401) {
+          bool isError401 = e.response!.statusCode == 401;
+          bool withToken =
+              e.requestOptions.headers["Authorization"] != "Bearer null";
+
+          if (isError401 && withToken) {
             bool getNewToken = await _refreshToken();
 
             if (getNewToken) {
